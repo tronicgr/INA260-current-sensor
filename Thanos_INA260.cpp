f6eb3d5a97981931a9d59f897c27e16facdcf88f
tronicgr <tronicgr@gmail.com>
2017-05-01 14:29:31 -0700

    Add files via upload

---
+++ Thanos_INA260.cpp
@@ -0,0 +1,184 @@
+/**************************************************************************/
+/*! 
+    @file     Thanos_INA260.cpp
+    @author   Thanos Kontogiannis 
+	@license  BSD (see license.txt)
+	
+	Driver for the INA260 current sensor
+
+	This is a library for the INA260 
+	
+		
+	@section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+#include "Thanos_INA260.h"
+
+/**************************************************************************/
+/*! 
+    @brief  Sends a single command byte over I2C
+*/
+/**************************************************************************/
+void Thanos_INA260::wireWriteRegister (uint8_t reg, uint16_t value)
+{
+  Wire.beginTransmission(ina260_i2caddr);
+  #if ARDUINO >= 100
+    Wire.write(reg);                       // Register
+    Wire.write((value >> 8) & 0xFF);       // Upper 8-bits
+    Wire.write(value & 0xFF);              // Lower 8-bits
+  #else
+    Wire.send(reg);                        // Register
+    Wire.send(value >> 8);                 // Upper 8-bits
+    Wire.send(value & 0xFF);               // Lower 8-bits
+  #endif
+  Wire.endTransmission();
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Reads a 16 bit values over I2C
+*/
+/**************************************************************************/
+void Thanos_INA260::wireReadRegister(uint8_t reg, uint16_t *value)
+{
+
+  Wire.beginTransmission(ina260_i2caddr);
+  #if ARDUINO >= 100
+    Wire.write(reg);                       // Register
+  #else
+    Wire.send(reg);                        // Register
+  #endif
+  Wire.endTransmission();
+  
+  delay(1); // Max 12-bit conversion time is 586us per sample
+
+  Wire.requestFrom(ina260_i2caddr, (uint8_t)2);  
+  #if ARDUINO >= 100
+    // Shift values to create properly formed integer
+    *value = ((Wire.read() << 8) | Wire.read());
+  #else
+    // Shift values to create properly formed integer
+    *value = ((Wire.receive() << 8) | Wire.receive());
+  #endif
+}
+
+
+/**************************************************************************/
+/*! 
+    @brief  Configures to INA260
+			
+*/
+/**************************************************************************/
+void Thanos_INA260::setConfigRegister(void)
+{
+  
+  
+    
+  // Set Config register 
+  uint16_t config = INA260_CONFIG_AVGRANGE_4 |
+                    INA260_CONFIG_BVOLTAGETIME_8244US |
+                    INA260_CONFIG_SCURRENTTIME_8244US |
+                    INA260_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
+  wireWriteRegister(INA260_REG_CONFIG, config);
+}
+
+
+/**************************************************************************/
+/*! 
+    @brief  Instantiates a new INA219 class
+*/
+/**************************************************************************/
+Thanos_INA260::Thanos_INA260(uint8_t addr) {
+  ina260_i2caddr = addr;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Setups the HW 
+*/
+/**************************************************************************/
+void Thanos_INA260::begin(uint8_t addr) {
+  ina260_i2caddr = addr;
+  begin();
+}
+
+void Thanos_INA260::begin(void) {
+  Wire.begin();    
+  // Set chip to large range config values to start
+  setConfigRegister();
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the raw bus voltage (16-bit signed integer, so +-32767)
+*/
+/**************************************************************************/
+int16_t Thanos_INA260::getBusVoltage_raw() {
+  uint16_t value;
+  wireReadRegister(INA260_REG_BUSVOLTAGE, &value);
+  return (int16_t)value;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the raw shunt voltage (16-bit signed integer, so +-32767)
+*/
+/**************************************************************************/
+int16_t Thanos_INA260::getShuntVoltage_raw() {
+  uint16_t value;
+  wireReadRegister(INA260_REG_SHUNTVOLTAGE, &value);
+  return (int16_t)value;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the raw current value (16-bit signed integer, so +-32767)
+*/
+/**************************************************************************/
+int16_t Thanos_INA260::getCurrent_raw() {
+  uint16_t value;
+  wireReadRegister(INA260_REG_CURRENT, &value);
+  return (int16_t)value;
+}
+ 
+/**************************************************************************/
+/*! 
+    @brief  Gets the shunt voltage in mV (so +-327mV)
+*/
+/**************************************************************************/
+float Thanos_INA260::getShuntVoltage_mV() {
+  int16_t value;
+  value = getShuntVoltage_raw();
+  return value * 0.007;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the shunt voltage in volts
+*/
+/**************************************************************************/
+float Thanos_INA260::getBusVoltage_V() {
+  int16_t value = getBusVoltage_raw();
+  return value * 0.00125;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Gets the current value in mA, taking into account the
+            config settings and current LSB
+*/
+/**************************************************************************/
+float Thanos_INA260::getCurrent_mA() {
+  float valueDec = getCurrent_raw();
+  return valueDec * 1.25;
+}
---
+++ Thanos_INA260.h
@@ -0,0 +1,124 @@
+/**************************************************************************/
+/*! 
+    @file     Thanos-INA260.h
+    @author   Thanos Kontogiannis (GA-ASI)
+	@license  BSD (see license.txt)
+		
+	
+
+    v1.0  - First release
+*/
+/**************************************************************************/
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define INA260_ADDRESS                         (0x40)    // 1000000 (A0+A1=GND)
+    #define INA260_READ                            (0x01)
+/*=========================================================================*/
+
+/*=========================================================================
+    CONFIG REGISTER (R/W)
+    -----------------------------------------------------------------------*/
+    #define INA260_REG_CONFIG                      (0x00)
+    /*---------------------------------------------------------------------*/
+    #define INA260_CONFIG_RESET                    (0x8000)  // Reset Bit
+	
+	#define INA260_CONFIG_AVGRANGE_MASK      	   (0x0E00)  // Averaging mode Mask
+	#define INA260_CONFIG_AVGRANGE_1               (0x0000)  // Average mode 1
+	#define INA260_CONFIG_AVGRANGE_4               (0x0200)  // Average mode 4
+	#define INA260_CONFIG_AVGRANGE_16              (0x0400)  // Average mode 16
+	#define INA260_CONFIG_AVGRANGE_64              (0x0600)  // Average mode 64
+	#define INA260_CONFIG_AVGRANGE_128             (0x0800)  // Average mode 128
+	#define INA260_CONFIG_AVGRANGE_256             (0x0A00)  // Average mode 256
+	#define INA260_CONFIG_AVGRANGE_512             (0x0C00)  // Average mode 512
+	#define INA260_CONFIG_AVGRANGE_1024            (0x0E00)  // Average mode 1024
+	
+	
+	#define INA260_CONFIG_BVOLTAGETIME_MASK        (0x01C0)  // Bus Voltage Conversion Time Mask
+	#define INA260_CONFIG_BVOLTAGETIME_140US       (0x0000)  // 140us
+	#define INA260_CONFIG_BVOLTAGETIME_1100US      (0x0100)  // 1.1ms
+	#define INA260_CONFIG_BVOLTAGETIME_8244US      (0x01C0)  // 8.244ms
+		
+	#define INA260_CONFIG_SCURRENTTIME_MASK        (0x0038)  // Shunt Current Conversion Time Mask
+	#define INA260_CONFIG_SCURRENTTIME_140US       (0x0000)  // 140us
+	#define INA260_CONFIG_SCURRENTTIME_1100US      (0x0020)  // 1.1ms
+	#define INA260_CONFIG_SCURRENTTIME_8244US      (0x0038)  // 8.244ms
+	
+		
+	#define INA260_CONFIG_MODE_MASK                (0x0007)  // Operating Mode Mask
+    #define INA260_CONFIG_MODE_POWERDOWN           (0x0000)
+    #define INA260_CONFIG_MODE_SVOLT_TRIGGERED     (0x0001)
+    #define INA260_CONFIG_MODE_BVOLT_TRIGGERED     (0x0002)
+    #define INA260_CONFIG_MODE_SANDBVOLT_TRIGGERED (0x0003)
+    #define INA260_CONFIG_MODE_ADCOFF              (0x0004)
+    #define INA260_CONFIG_MODE_SVOLT_CONTINUOUS    (0x0005)
+    #define INA260_CONFIG_MODE_BVOLT_CONTINUOUS    (0x0006)
+    #define INA260_CONFIG_MODE_SANDBVOLT_CONTINUOUS (0x0007)	
+	
+	
+/*=========================================================================*/
+
+/*=========================================================================
+    SHUNT VOLTAGE REGISTER (R)
+    -----------------------------------------------------------------------*/
+    #define INA260_REG_SHUNTVOLTAGE                (0x01)
+/*=========================================================================*/
+
+/*=========================================================================
+    BUS VOLTAGE REGISTER (R)
+    -----------------------------------------------------------------------*/
+    #define INA260_REG_BUSVOLTAGE                  (0x02)
+/*=========================================================================*/
+
+/*=========================================================================
+    POWER REGISTER (R)
+    -----------------------------------------------------------------------*/
+    #define INA260_REG_POWER                       (0x03)
+/*=========================================================================*/
+
+/*=========================================================================
+    CURRENT REGISTER (R)
+    -----------------------------------------------------------------------*/
+    #define INA260_REG_CURRENT                     (0x01)
+/*=========================================================================*/
+
+/*=========================================================================
+    CALIBRATION REGISTER (R/W)
+    -----------------------------------------------------------------------*/
+    #define INA260_MASK_ENABLE                     (0x06)
+/*=========================================================================*/
+
+class Thanos_INA260{
+ public:
+  Thanos_INA260(uint8_t addr = INA260_ADDRESS);
+  void begin(void);
+  void begin(uint8_t addr);
+  //void setCalibration_32V_5A(void);
+  void setConfigRegister(void);
+  //void setCalibration_32V_1A(void);
+  //void setCalibration_16V_400mA(void);
+  float getBusVoltage_V(void);
+  float getShuntVoltage_mV(void);
+  float getCurrent_mA(void);
+
+ private:
+  uint8_t ina260_i2caddr;
+  //uint32_t ina260_calValue;
+  // The following multipliers are used to convert raw current and power
+  // values to mA and mW, taking into account the current config settings
+   
+  void wireWriteRegister(uint8_t reg, uint16_t value);
+  void wireReadRegister(uint8_t reg, uint16_t *value);
+  int16_t getBusVoltage_raw(void);
+  int16_t getShuntVoltage_raw(void);
+  int16_t getCurrent_raw(void);
+};
